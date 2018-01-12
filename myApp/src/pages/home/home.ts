f9064f7820d26c29045219ef200c4dc617234690
Teodor Tripon <teodor.tripon@accesa.eu>
2018-01-12 10:08:03 +0200

    integration with faceapi succesful tested with webcam in broser mode

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -24,21 +24,45 @@ export class HomePage {
     saveToPhotoAlbum: true
   }
 
+public resdata: Object;
   takePicture() {
     this.camera.getPicture(this.options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
+
+      var makeblob = function (dataURL) {
+        var BASE64_MARKER = ';base64,';
+        if (dataURL.indexOf(BASE64_MARKER) == -1) {
+            var parts = dataURL.split(',');
+            var contentType = parts[0].split(':')[1];
+            var raw = decodeURIComponent(parts[1]);
+            return new Blob([raw], { type: contentType });
+        }
+        var parts = dataURL.split(BASE64_MARKER);
+        var contentType = parts[0].split(':')[1];
+        var raw = window.atob(parts[1]);
+        var rawLength = raw.length;
     
-     // var mImg = { url: "https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg" };
-      var blobimg = fetch(base64Image ) .then(res => res.blob());
+        var uInt8Array = new Uint8Array(rawLength);
+    
+        for (var i = 0; i < rawLength; ++i) {
+            uInt8Array[i] = raw.charCodeAt(i);
+        }
+    
+        return new Blob([uInt8Array], { type: contentType });
+    }
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      var blobimg = makeblob(base64Image);
         console.log(blobimg);
-      this.faceApiService.detectFace(blobimg).then(res => { console.log(res) });
+      this.faceApiService.detectFace(blobimg).then(res => { console.log(res);this.resdata = res; });
     }, (err) => {
       // Handle error
       console.log(err);
     });
   }
+
+
+
 }
 
 
